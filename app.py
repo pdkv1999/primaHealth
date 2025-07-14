29bfb3d7bb7e1615d2b2390d031083c2b8f5597c
Dileep <dileep@Dileeps-MacBook-Air.local>
2025-07-14 11:31:31 +0100

    Push screens

--- app.py
+++ app.py
@@ -10,5 +10,13 @@ doctor_notes = []
 def index():
     return render_template('home.html')
 
+@app.route('/phase1')
+def phase1():
+    return render_template('phase1.html')
+
+@app.route('/ap_screen')
+def ap_screen():
+    return render_template('ap_screen.html')  # Make sure this HTML file exists in the templates folder
+
 if __name__ == '__main__':
     app.run(debug=True)
